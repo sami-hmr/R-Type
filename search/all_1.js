327be807af75a521a7cbb80fb43335b6aa0b78c7
SIMLUKE <SIMLUKE@users.noreply.github.com>
2025-11-24 17:28:37 +0000

    deploy: a0f19415c76ce13d84cdc9a42ba7553c55ac96fd

--- search/all_1.js
+++ search/all_1.js
@@ -3,9 +3,8 @@ var searchData=
   ['about_0',['About',['../about.html',1,'']]],
   ['about_2edox_1',['about.dox',['../about_8dox.html',1,'']]],
   ['add_5fcomponent_2',['add_component',['../classRegistery.html#ae9f04a970988021bfcdd86327dbb2a5c',1,'Registery']]],
-  ['add_5fsystem_3',['add_system',['../classRegistery.html#ab2bc8e4952704bb818edcc2385b1a7a3',1,'Registery']]],
-  ['addsystem_4',['addSystem',['../classRegistery.html#a3b4423d3d293cf5ca1cba2c9eb8903fe',1,'Registery']]],
-  ['aplugin_5',['aplugin',['../classAPlugin.html',1,'APlugin'],['../classAPlugin.html#a9f249933208731257f83fb665f9e0a77',1,'APlugin::APlugin()']]],
-  ['aplugin_2ehpp_6',['APlugin.hpp',['../APlugin_8hpp.html',1,'']]],
-  ['available_20presets_7',['Available Presets',['../index.html#autotoc_md3',1,'']]]
+  ['addsystem_3',['addSystem',['../classRegistery.html#a3b4423d3d293cf5ca1cba2c9eb8903fe',1,'Registery']]],
+  ['aplugin_4',['aplugin',['../classAPlugin.html',1,'APlugin'],['../classAPlugin.html#a9f249933208731257f83fb665f9e0a77',1,'APlugin::APlugin()']]],
+  ['aplugin_2ehpp_5',['APlugin.hpp',['../APlugin_8hpp.html',1,'']]],
+  ['available_20presets_6',['Available Presets',['../index.html#autotoc_md3',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['add_5fcomponent_0',['add_component',['../classRegistery.html#ae9f04a970988021bfcdd86327dbb2a5c',1,'Registery']]],
-  ['add_5fsystem_1',['add_system',['../classRegistery.html#ab2bc8e4952704bb818edcc2385b1a7a3',1,'Registery']]],
-  ['addsystem_2',['addSystem',['../classRegistery.html#a3b4423d3d293cf5ca1cba2c9eb8903fe',1,'Registery']]],
-  ['aplugin_3',['APlugin',['../classAPlugin.html#a9f249933208731257f83fb665f9e0a77',1,'APlugin']]]
+  ['addsystem_1',['addSystem',['../classRegistery.html#a3b4423d3d293cf5ca1cba2c9eb8903fe',1,'Registery']]],
+  ['aplugin_2',['APlugin',['../classAPlugin.html#a9f249933208731257f83fb665f9e0a77',1,'APlugin']]]
 ];
