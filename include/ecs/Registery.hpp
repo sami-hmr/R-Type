0e2e089bb77a57ca1bc37d489d918d22d74d28b7
Sami <sami12022005@yahoo.fr>
2025-11-25 14:39:45 +0100

    doc: registry and sparse array docstring. cmake --build build/ --target docs to build the doc

--- include/ecs/Registery.hpp
+++ include/ecs/Registery.hpp
@@ -11,11 +11,26 @@
 
 #include "SparseArray.hpp"
 
+/**
+ * @brief The Registery class is the core of the ECS (Entity-Component-System) architecture.
+ * 
+ * It manages entities, their associated components, and the systems that operate on them.
+ */
 class Registery
 {
 public:
+  /**
+   * @brief Type alias for an entity identifier.
+   * 
+   */
   using Entity = std::size_t;
 
+  /**
+   * @brief Registers a component type in the registry.
+   * 
+   * @tparam Component The type of the component to register.
+   * @return SparseArray<Component>& A reference to the sparse array of the registered component type.
+   */
   template<class Component>
   SparseArray<Component>& register_component()
   {
@@ -27,6 +42,12 @@ public:
     return comp;
   }
 
+  /**
+   * @brief Get the components object
+   * 
+   * @tparam Component The type of the component to retrieve.
+   * @return SparseArray<Component>& A reference to the sparse array of the specified component type.
+   */
   template<class Component>
   SparseArray<Component>& get_components()
   {
@@ -34,6 +55,12 @@ public:
         this->_components.at(std::type_index(typeid(Component))));
   }
 
+  /**
+   * @brief Get the components object (const version)
+   * 
+   * @tparam Component The type of the component to retrieve.
+   * @return SparseArray<Component> const& A const reference to the sparse array of the specified component type.
+   */
   template<class Component>
   SparseArray<Component> const& get_components() const
   {
@@ -41,6 +68,12 @@ public:
         this->_components.at(std::type_index(typeid(Component))));
   }
 
+  /**
+   * @brief Spawns a new entity.
+   * If there are any previously killed entities, it reuses their IDs.
+   *
+   * @return Entity: The ID of the spawned entity.
+   */
   Entity spawn_entity()
   {
     Entity to_return = 0;
@@ -54,6 +87,11 @@ public:
     return to_return;
   }
 
+  /**
+   * @brief Kills an entity, marking it for deletion and allowing its ID to be reused.
+   * 
+   * @param e The entity to kill.
+   */
   void kill_entity(Entity const& e)
   {
     for (auto const& f : this->_delete_functions) {
@@ -62,6 +100,14 @@ public:
     this->_dead_entites.push(e);
   }
 
+  /**
+   * @brief Adds a component to an entity.
+   * 
+   * @tparam Component The type of the component to add.
+   * @param to The entity to which the component will be added.
+   * @param c The component to add.
+   * @return SparseArray<Component>::Ref A reference to the added component.
+   */
   template<typename Component>
   typename SparseArray<Component>::Ref add_component(Entity const& to,
                                                      Component&& c)
@@ -70,6 +116,15 @@ public:
         to, std::forward<Component>(c));
   }
 
+  /**
+   * @brief Constructs and adds a component to an entity.
+   * 
+   * @tparam Component The type of the component to add.
+   * @tparam Params The types of the parameters to construct the component.
+   * @param to The entity to which the component will be added.
+   * @param p The parameters to construct the component.
+   * @return SparseArray<Component>::Ref A reference to the added component.
+   */
   template<typename Component, typename... Params>
   typename SparseArray<Component>::Ref emplace_component(Entity const& to,
                                                          Params&&... p)
@@ -78,12 +133,25 @@ public:
         to, std::forward<Params>(p)...);
   }
 
+  /**
+   * @brief Removes a component from an entity.
+   * 
+   * @tparam Component: The type of the component to remove.
+   * @param from The entity from which the component will be removed.
+   */
   template<typename Component>
   void remove_component(Entity const& from)
   {
     this->get_components<Component>().erase(from);
   }
 
+  /**
+   * @brief Adds a system that operates on specified components.
+   * 
+   * @tparam Components: The types of the components the system will operate on.
+   * @tparam Function The type of the function representing the system.
+   * @param f The function representing the system.
+   */
   template<class... Components, typename Function>
   void add_system(Function const& f)
   {
@@ -91,6 +159,10 @@ public:
         [this, f]() { f(*this, this->get_components<Components>()...); });
   }
 
+  /**
+   * @brief Runs all registered systems.
+   * 
+   */
   void run_systems()
   {
     for (auto const& f : this->_frequent_systems) {
@@ -99,9 +171,9 @@ public:
   }
 
 private:
-  std::unordered_map<std::type_index, std::any> _components;
-  std::vector<std::function<void()>> _frequent_systems;
-  std::vector<std::function<void(Entity const&)>> _delete_functions;
-  std::queue<Entity> _dead_entites;
-  std::size_t _max = 0;
+  std::unordered_map<std::type_index, std::any> _components; /**< Stores components indexed by their type */
+  std::vector<std::function<void()>> _frequent_systems; /**< Stores systems to be run frequently */
+  std::vector<std::function<void(Entity const&)>> _delete_functions; /**< Stores functions to delete components when an entity is killed */
+  std::queue<Entity> _dead_entites; /**< Stores IDs of entities that have been killed and can be reused */
+  std::size_t _max = 0; /**< Tracks the maximum entity ID assigned so far */
 };
