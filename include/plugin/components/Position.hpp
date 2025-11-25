8247c0c25012a7618344f3ce368b940ef5a73401
FireSky33 <gauthier.fagot@epitech.eu>
2025-11-25 16:30:31 +0100

    feat(components): add component Position in individual file

---
+++ include/plugin/components/Position.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Position
+*/
+
+#pragma once
+
+struct Position
+{
+  Position(float x, float y)
+      : x(x)
+      , y(y)
+  {
+  }
+
+  double x;
+  double y;
+};
+
