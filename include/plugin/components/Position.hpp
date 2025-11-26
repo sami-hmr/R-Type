9cda4092dafe63acc00596722300b7d8e4838ab6
FireSky33 <gauthier.fagot@epitech.eu>
2025-11-26 22:56:57 +0100

    feat(collision): create Collision component

--- include/plugin/components/Position.hpp
+++ include/plugin/components/Position.hpp
@@ -1,10 +1,3 @@
-/*
-** EPITECH PROJECT, 2025
-** R-Type
-** File description:
-** Position
-*/
-
 #pragma once
 
 struct Position
--- include/plugin/components/Sprite.hpp
+++ include/plugin/components/Sprite.hpp
@@ -1,10 +1,3 @@
-/*
-** EPITECH PROJECT, 2025
-** R-Type
-** File description:
-** Sprite
-*/
-
 #pragma once
 
 #include <string>
