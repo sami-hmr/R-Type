aafffc7d19d108a14be327501ed5d0e3d33baade
Idriss Dupoisot <idriss.akbi@epitech.eu>
2025-12-02 10:47:51 +0100

    fix: added details in protocol UDP - using macros to make it cleaner and easier to change values like the magic sequence, current protocol version etc - added circular buffer reader for endpoint (clients)

--- plugins/server_network/include/Network.hpp
+++ plugins/server_network/include/Network.hpp
@@ -21,9 +21,14 @@
 #include "plugin/EntityLoader.hpp"
 
 #define MAX_PLAYERS 4
+#define BUFFER_SIZE 2048
 #define MAGIC_SEQUENCE 0x67676767
+#define MAGIC_LENGTH 4
+#define SLEEP_DURATION 10
+#define UNUSED __attribute__((unused))
+#define CURRENT_PROTOCOL_VERSION 1
 
-enum class ClientState
+enum class ClientState : std::uint8_t
 {
   DISCONNECTED,
   CHALLENGING,
@@ -42,47 +47,47 @@ struct ClientInfo
 
 class NetworkServer : public APlugin
 {
-public:
-  NetworkServer(Registery& r, EntityLoader& l);
-  ~NetworkServer() override;
+  public:
+    NetworkServer(Registery& r, EntityLoader& l);
+    ~NetworkServer() override;
 
-private:
-  void launch_server(ServerLaunching const& s);
-  void receive_loop();
+  private:
+    void launch_server(ServerLaunching const& s);
+    void receive_loop();
 
-  void handle_connectionless_packet(const std::string& command,
-                                    const asio::ip::udp::endpoint& sender);
-  void send_connectionless(const std::string& response,
-                           const asio::ip::udp::endpoint& endpoint);
+    void handle_connectionless_packet(const std::string& command,
+                                      const asio::ip::udp::endpoint& sender);
+    void send_connectionless(const std::string& response,
+                            const asio::ip::udp::endpoint& endpoint);
 
-  void handle_getinfo(const std::vector<std::string>& args,
-                      const asio::ip::udp::endpoint& sender);
-  void handle_getstatus(const std::vector<std::string>& args,
+    void handle_getinfo(const std::vector<std::string>& args,
                         const asio::ip::udp::endpoint& sender);
-  void handle_getchallenge(const std::vector<std::string>& args,
-                           const asio::ip::udp::endpoint& sender);
-  void handle_connect(const std::vector<std::string>& args,
-                      const asio::ip::udp::endpoint& sender);
+    void handle_getstatus(const std::vector<std::string>& args,
+                          const asio::ip::udp::endpoint& sender);
+    void handle_getchallenge(const std::vector<std::string>& args,
+                            const asio::ip::udp::endpoint& sender);
+    void handle_connect(const std::vector<std::string>& args,
+                        const asio::ip::udp::endpoint& sender);
 
-  uint32_t generate_challenge();
-  std::vector<std::string> parse_args(const std::string& command);
-  ClientInfo* find_client_by_endpoint(const asio::ip::udp::endpoint& endpoint);
+    uint32_t generate_challenge();
+    std::vector<std::string> parse_args(const std::string& command);
+    ClientInfo* find_client_by_endpoint(const asio::ip::udp::endpoint& endpoint);
 
-  static const std::unordered_map<std::string,
-                                  void (NetworkServer::*)(
-                                      const std::vector<std::string>&,
-                                      const asio::ip::udp::endpoint&)>
-      _command_table;
+    static const std::unordered_map<std::string,
+      void (NetworkServer::*)(
+        const std::vector<std::string>&,
+        const asio::ip::udp::endpoint&)>
+          _command_table;
 
-  asio::io_context _io_c;
-  std::unique_ptr<asio::ip::udp::socket> _socket;
-  std::vector<std::thread> _threads;
-  std::array<ClientInfo, MAX_PLAYERS> _clients;
-  CircularBuffer<2048> _recv_buffer;
-  uint32_t _server_id;
-  bool _running = false;
+    asio::io_context _io_c;
+    std::unique_ptr<asio::ip::udp::socket> _socket;
+    std::vector<std::thread> _threads;
+    std::array<ClientInfo, MAX_PLAYERS> _clients;
+    CircularBuffer<BUFFER_SIZE> _recv_buffer;
+    uint32_t _server_id;
+    bool _running = false;
 
-  std::string _hostname = "R-Type Server";
-  std::string _mapname = "level1";
-  int _max_players = MAX_PLAYERS;
+    std::string _hostname = "R-Type Server";
+    std::string _mapname = "level1";
+    int _max_players = MAX_PLAYERS;
 };
