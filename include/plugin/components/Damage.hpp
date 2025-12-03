4dc18aeeaca327ecdc77782755639eb109509431
SIMLUKE <luc.simon@epitech.eu>
2025-12-03 15:32:37 +0100

    fix(components): added byte parsing to heal and damage

--- include/plugin/components/Damage.hpp
+++ include/plugin/components/Damage.hpp
@@ -1,13 +1,20 @@
 #pragma once
+#include "ByteParser/ByteParser.hpp"
+#include "plugin/Byte.hpp"
 
 struct Damage
 {
-    Damage() = default;
-    
-    Damage(int amount)
-        : amount(amount)
-    {
-    }
+  Damage() = default;
 
-    int amount;
+  Damage(int amount)
+      : amount(amount)
+  {
+  }
+
+  DEFAULT_BYTE_CONSTRUCTOR(Damage,
+                           ([](int x) { return (Damage) {x}; }),
+                           parseByte<int>())
+  DEFAULT_SERIALIZE(type_to_byte(this->amount))
+
+  int amount;
 };
--- include/plugin/components/Heal.hpp
+++ include/plugin/components/Heal.hpp
@@ -1,13 +1,20 @@
 #pragma once
+#include "ByteParser/ByteParser.hpp"
+#include "plugin/Byte.hpp"
 
 struct Heal
 {
-    Heal() = default;
-    
-    Heal(int amount)
-        : amount(amount)
-    {
-    }
+  Heal() = default;
 
-    int amount;
+  Heal(int amount)
+      : amount(amount)
+  {
+  }
+
+  DEFAULT_BYTE_CONSTRUCTOR(Heal,
+                           ([](int x) { return (Heal) {x}; }),
+                           parseByte<int>())
+  DEFAULT_SERIALIZE(type_to_byte(this->amount))
+
+  int amount;
 };
