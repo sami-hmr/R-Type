a4872b9d2de3cff0aef36fe91d43ee47e8648549
FireSky33 <gauthier.fagot@epitech.eu>
2025-12-04 21:58:29 +0100

    feat(components): create Follower component. Defines an entity that follows a target

---
+++ include/plugin/components/Follower.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstddef>
+
+#include "ParserUtils.hpp"
+#include "plugin/Byte.hpp"
+#include "plugin/Hooks.hpp"
+
+struct Follower
+{
+  Follower() = default;
+
+  Follower(std::size_t target, bool lost_target)
+      : target(target)
+      , lost_target(lost_target)
+  {
+  }
+
+  Follower(std::size_t target)
+      : target(target)
+      , lost_target(false)
+  {
+  }
+
+  DEFAULT_BYTE_CONSTRUCTOR(Follower,
+                           ([](std::size_t target, bool lost_target)
+                            { return (Follower) {target, lost_target}; }),
+                           parseByte<std::size_t>(),
+                           parseByte<bool>())
+  DEFAULT_SERIALIZE(type_to_byte(this->target),
+                    type_to_byte(this->lost_target))
+
+  HOOKABLE(Follower, HOOK(target), HOOK(lost_target))
+  
+
+  std::size_t target;
+  bool lost_target;
+};
