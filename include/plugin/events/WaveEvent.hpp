644f9cc351e07fd8aaea91b2dd6c0edb20f6d67f
FireSky33 <gauthier.fagot@epitech.eu>
2026-01-09 01:25:57 +0100

    feat(event): add WaveSpawnEvent that load a wave entity in game

---
+++ include/plugin/events/WaveEvent.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ByteParser/ByteParser.hpp"
+#include "Json/JsonParser.hpp"
+#include "ecs/Registry.hpp"
+#include "plugin/Byte.hpp"
+#include "plugin/Hooks.hpp"
+#include "plugin/events/EventMacros.hpp"
+#include "ParserUtils.hpp"
+#include <string>
+#include <utility>
+
+struct WaveSpawnEvent
+{
+  WaveSpawnEvent() = default;
+
+  WaveSpawnEvent(std::string wave_temp)
+      : wave_template(std::move(wave_temp))
+  {
+  }
+
+  CHANGE_ENTITY_DEFAULT
+
+  DEFAULT_BYTE_CONSTRUCTOR(WaveSpawnEvent,
+                           ([](std::string const& wave_temp) { return WaveSpawnEvent(wave_temp); }),
+                           parseByteString())
+
+  DEFAULT_SERIALIZE(string_to_byte(wave_template))
+
+  WaveSpawnEvent(Registry& r, JsonObject const& conf)
+      : wave_template(*get_value_copy<std::string>(r, conf, "wave_template"))
+  {
+  }
+
+  std::string wave_template;
+};
