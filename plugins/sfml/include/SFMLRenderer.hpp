0fe02bd11fe95874d77d4ad1ca04ae468c0d6b69
Sami <sami12022005@yahoo.fr>
2026-01-09 21:50:52 +0100

    feat: vertex_buffer in rederer class

--- plugins/sfml/include/SFMLRenderer.hpp
+++ plugins/sfml/include/SFMLRenderer.hpp
@@ -8,11 +8,14 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/VertexBuffer.hpp>
 #include <SFML/Graphics/View.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
@@ -101,6 +104,8 @@ private:
   std::optional<sf::Text> _text;
   sf::RectangleShape _rectangle;
   sf::CircleShape _circle;
+  sf::VertexBuffer _vertex_buffer;
+  int _current_vertex = 0;
 
   sf::View _view;
   bool _camera_initialized = false;
