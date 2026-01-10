282189b685fc6fee68324cc21d47dcc5190468d7
Idriss Dupoisot <idriss.akbi@epitech.eu>
2026-01-10 09:50:12 +0100

    fix: inventory should be able to emit events now on throw/consume

--- plugins/inventory/include/Inventory.hpp
+++ plugins/inventory/include/Inventory.hpp
@@ -3,8 +3,9 @@
 #include "ecs/Registry.hpp"
 #include "plugin/APlugin.hpp"
 #include "plugin/components/Item.hpp"
+#include "plugin/events/InventoryEvents.hpp"
 
-
+#define DFLT_MAX 64
 class Inventory : public APlugin
 {
 public:
@@ -16,10 +17,13 @@ private:
   std::vector<std::pair<Item, std::size_t>> _inventory;
   std::size_t _max_items;
 
-  void use_item(std::uint8_t slot, std::size_t nb, Registry::Entity e);
+  void use_item(std::uint8_t slot, std::size_t nb);
   void add_item(const Item& item, std::size_t nb);
 
   void init_item_vector(Registry::Entity const& entity, JsonArray& inventory);
 
   void init_inventory(Registry::Entity const& entity, JsonObject const& obj);
+
+  template<typename T>
+  bool usage_emit(const ItemEvent<T>& event, std::string area);
 };
--- plugins/inventory/src/Inventory.cpp
+++ plugins/inventory/src/Inventory.cpp
@@ -1,25 +1,69 @@
 #include "Inventory.hpp"
 
+#include "ecs/Registry.hpp"
+#include "ecs/EmitEvent.hpp"
+#include "plugin/APlugin.hpp"
 #include "Json/JsonParser.hpp"
 #include "ecs/EventManager.hpp"
-#include "ecs/Registry.hpp"
-#include "plugin/APlugin.hpp"
 #include "plugin/EntityLoader.hpp"
 #include "plugin/components/Item.hpp"
-#include "plugin/events/InventoryEvents.hpp"
 #include "plugin/events/LoggerEvent.hpp"
+#include "plugin/events/InventoryEvents.hpp"
+
+template<typename T>
+bool Inventory::usage_emit(const ItemEvent<T>& event, std::string area)
+{
+  use_item(event.slot_item, event.nb_to_use);
+  auto use_item = get_value_copy<JsonObject>(
+      this->_registry.get(),
+      _inventory[event.slot_item].first.object.second,
+      area);
+  auto entity =
+      get_value_copy<int>(this->_registry.get(),
+                          _inventory[event.slot_item].first.object.second,
+                          "entity");
+  if (!use_item || !entity) {
+    LOGGER("Inventory",
+           LogLevel::ERROR,
+           std::format(
+               "Missing {} field in item. No animation nor event played", area));
+    return false;
+  }
+  auto evt_use =
+      get_value_copy<JsonObject>(this->_registry.get(), *use_item, "event");
+  if (evt_use) {
+    auto name =
+        get_value_copy<std::string>(this->_registry.get(), *evt_use, "name");
+    auto params =
+        get_value_copy<JsonObject>(this->_registry.get(), *evt_use, "params");
+    if (!name || !params) {
+      LOGGER(
+          "Inventory",
+          LogLevel::ERROR,
+          std::format(
+              "Invalid event field in item's {} configuration. No animation nor even't played", area));
+      return false;
+    }
+    params->insert_or_assign("entity", JsonValue(*entity));
+    emit_event(
+        this->_event_manager.get(), this->_registry.get(), *name,
+        *params);
+  }
+  return false;
+}
 
 Inventory::Inventory(Registry& r, EventManager& em, EntityLoader& l)
     : APlugin(
           "inventory", r, em, l, {}, {COMP_INIT(Item, Item, init_inventory)})
     , entity_loader(l)
+    , _max_items(DFLT_MAX)
 {
   SUBSCRIBE_EVENT(Consume, {
     if (event.slot_item >= _inventory.size()) {
       return false;
     }
     if (_inventory[event.slot_item].first.consumable) {
-      use_item(event.slot_item, event.nb_to_use, event.consumer);
+      return usage_emit<ConsumeItem>(event, "consume");
     }
   })
   SUBSCRIBE_EVENT(Throw, {
@@ -27,58 +71,14 @@ Inventory::Inventory(Registry& r, EventManager& em, EntityLoader& l)
       return false;
     }
     if (_inventory[event.slot_item].first.throwable) {
-      use_item(event.slot_item, event.nb_to_use, event.consumer);
-      auto throw_item = get_value_copy<JsonObject>(
-          this->_registry.get(),
-          _inventory[event.slot_item].first.object.second,
-          "throw");
-      auto entity =
-          get_value_copy<int>(this->_registry.get(),
-                              _inventory[event.slot_item].first.object.second,
-                              "entity");
-      if (!throw_item || !entity) {
-        LOGGER(
-            "Inventory",
-            LogLevel::ERROR,
-            std::format(
-                "Missing throw field in item. No animation nor event played"));
-        return false;
-      }
-      auto evt_throw = get_value_copy<JsonObject>(
-          this->_registry.get(), *throw_item, "event");
-      if (evt_throw) {
-        auto name = get_value_copy<std::string>(
-            this->_registry.get(), *evt_throw, "name");
-        auto params = get_value_copy<JsonObject>(
-            this->_registry.get(), *evt_throw, "params");
-        if (!name || !params) {
-          LOGGER(
-              "Inventory",
-              LogLevel::ERROR,
-              std::
-                  format(
-                      "Invalid event field in item's throw " "configuration. "
-                                                             "No " "animation "
-                                                                   "nor " "even"
-                                                                          "t "
-                                                                          "play"
-                                                                          "e"
-                                                                          "d"));
-          return false;
-        }
-        params->insert_or_assign("entity", JsonValue(*entity));
-        // emit_event(
-        //     this->_event_manager.get(), this->_registry.get(), *name,
-        //     *params);
-        return false;
-      }
+      return usage_emit<ThrowItem>(event, "throw");
     }
   })
   SUBSCRIBE_EVENT(Remove, {
     if (event.slot_item >= _inventory.size()) {
       return false;
     }
-    use_item(event.slot_item, event.nb_to_use, event.consumer);
+    use_item(event.slot_item, event.nb_to_use);
   })
   SUBSCRIBE_EVENT(PickUp, { add_item(event.item, event.nb_to_use); })
 }
@@ -98,34 +98,10 @@ void Inventory::init_item_vector(Registry::Entity const& entity,
         get_value_copy<std::size_t>(this->_registry.get(), item, "quantity");
     auto config =
         get_value_copy<JsonObject>(this->_registry.get(), item, "config");
-    if (!name) {
+    if (!name || !quantity || !consumable || !throwable || !config) {
       LOGGER("Inventory",
              LogLevel::WARNING,
-             std::format("Missing name field in item, skipping"));
-      continue;
-    }
-    if (!quantity) {
-      LOGGER("Inventory",
-             LogLevel::WARNING,
-             std::format("Missing quantity field in item, skipping"));
-      continue;
-    }
-    if (!consumable) {
-      LOGGER("Inventory",
-             LogLevel::WARNING,
-             std::format("Missing consumable field in item, skipping"));
-      continue;
-    }
-    if (!throwable) {
-      LOGGER("Inventory",
-             LogLevel::WARNING,
-             std::format("Missing throwable field in item, skipping"));
-      continue;
-    }
-    if (!config) {
-      LOGGER("Inventory",
-             LogLevel::WARNING,
-             std::format("Missing config field in item, skipping"));
+             std::format("Missing a field in item, skipping"));
       continue;
     }
     config->insert_or_assign("entity", JsonValue(static_cast<int>(entity)));
@@ -163,12 +139,11 @@ void Inventory::add_item(const Item& item, std::size_t nb)
   _inventory.emplace_back(item, nb);
 }
 
-void Inventory::use_item(std::uint8_t slot, std::size_t nb, Registry::Entity e)
+void Inventory::use_item(std::uint8_t slot, std::size_t nb)
 {
   if (nb < _inventory[slot].second) {
     _inventory[slot].second -= nb;
   } else {
     _inventory.erase(_inventory.begin() + slot);
   }
-  // complete
 }
