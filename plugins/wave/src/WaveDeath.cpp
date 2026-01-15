ea7d937477f50267f608c3b5e868aa0822a41395
FireSky33 <gauthier.fagot@epitech.eu>
2026-01-15 14:30:11 +0100

    fix: change scene to level_1 at start of the game

--- plugins/wave/src/WaveDeath.cpp
+++ plugins/wave/src/WaveDeath.cpp
@@ -3,13 +3,17 @@
 #include "plugin/components/Wave.hpp"
 
 #include "WaveManager.hpp"
+#include "ecs/EventManager.hpp"
 #include "ecs/Registry.hpp"
 #include "ecs/Scenes.hpp"
 #include "ecs/zipper/ZipperIndex.hpp"
+#include "plugin/Hooks.hpp"
 #include "plugin/components/Direction.hpp"
 #include "plugin/components/Position.hpp"
 #include "plugin/components/WaveTag.hpp"
 #include "plugin/events/EntityManagementEvent.hpp"
+#include "plugin/events/SceneChangeEvent.hpp"
+
 
 void WaveManager::wave_death_system(Registry& r)
 {
@@ -26,8 +30,24 @@ void WaveManager::wave_death_system(Registry& r)
       }
     }
     if (remaining == 0) {
-      this->_event_manager.get().emit(
-          this->_registry, wave.on_end.event_name, wave.on_end.params);
+      for (auto const &event: wave.on_end) {
+        if (event.event_name == "SceneChangeEvent") {
+          this->_event_manager.get().emit<SceneChangeEvent>("game", "", true);
+          this->_event_manager.get().emit<EventBuilderId>(
+              std::nullopt,
+              "SceneChangeEvent",
+              SceneChangeEvent("game", "", /*f=*/true).to_bytes());
+
+          this->_event_manager.get().emit<SceneChangeEvent>(event.event_name, "", false);
+          this->_event_manager.get().emit<EventBuilderId>(
+              std::nullopt,
+              "SceneChangeEvent",
+              SceneChangeEvent(event.event_name, "", /*f=*/false).to_bytes());
+        } else {
+          this->_event_manager.get().emit(
+            this->_registry, event.event_name, event.params);
+        }
+      }
       this->_event_manager.get().emit<DeleteEntity>(wave_entity);
     }
   }
